55d8acc9992d16094dabd99dbe4a72994e2de5a2
vemonet <vincent.emonet@gmail.com>
2019-08-20 10:51:32 +0200

    add start of test for drugbank ID (need to add SPARQL query)

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,13 +1,21 @@
 from behave import *
 
-@given('we have behave installed')
-def step_impl(context):
-    pass
+"""
+Given the dataset "https://w3id.org/data2services/graph/biolink/drugbank"
+When we get the entity "http://identifiers.org/drugbank:DB00001"
+Then its property "bl:name" should be "Lepirudin"
+"""
 
-@when('we implement a test')
-def step_impl(context):
-    assert True is not False
 
-@then('behave will test it for us!')
-def step_impl(context):
-    assert context.failed is False
+@given('the dataset "{graphUri}"')
+def step_impl(context, graphUri):
+    context.graphUri = graphUri
+
+@when('we get the entity "{entityUri}"')
+def step_impl(context, entityUri):
+    context.entityUri = entityUri
+
+@then('its property "{property}" should be "{value}"')
+def step_impl(context, property, value):
+    # Do SPARQL query
+    assert context.graphUri == "https://w3id.org/data2services/graph/biolink/drugbank"
--- features/tutorial.feature
+++ features/tutorial.feature
@@ -1,6 +1,11 @@
 Feature: showing off behave
 
-  Scenario: run a simple test
-     Given we have behave installed
-      When we implement a test
-      Then behave will test it for us!
+  # Scenario: run a simple test
+  #    Given we have behave installed
+  #     When we implement a test
+  #     Then behave will test it for us!
+
+  Scenario: Check for DrugBank drug name
+    Given the dataset "https://w3id.org/data2services/graph/biolink/drugbank"
+    When we get the entity "http://identifiers.org/drugbank:DB00001"
+    Then its property "bl:name" should be "Lepirudin"
